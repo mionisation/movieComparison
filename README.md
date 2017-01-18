0c168cbddb19af92781db099cc66445be19f99d0
Michael Ion <michael.antonius.ion@gmail.com>
2017-01-19 00:13:47 +0100

    Add changes to connector, now working and building dataset

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Small data wrangling and vis project for trying out the R Shiny framework
 
 todo:
 1. get movies, decide one of the two sets, get tomatoe ratings from omdb, maybe look for add. attributes (oscar, type of movie) for filtering for rating comparison
+
+
+1st step ))
+# IMDB SET LINKS: go to list page and click on the bottom (right under 'go to next page'): "Export this list" 
+
+All movie releases wide distr:
+http://www.imdb.com/list/ls057823854/
+
+2016 movie releases:
+http://www.imdb.com/list/ls076622175/
+
+2 ) get tomato rankings + metascore from omdb
--- movieAPIconnector.R
+++ movieAPIconnector.R
@@ -1,22 +1,28 @@
 require(RJSONIO)
 
 getOMDBMovieData <- function(movieSrcName) {
-  movie <- read.csv(file=movieSrcName, header = TRUE, sep = ',', quote = "\"")
+  movies <- read.csv(file=movieSrcName, header = TRUE, sep = ',', quote = "\"")
   #create omdb dataframe
-  dat <- getMovieFromOMDB(1);
+  dat <- getMovieFromOMDB(movies[1,2]);
   omdbDF <- as.data.frame(setNames(replicate(35,numeric(0), simplify = F), names(dat)))
-  for(i in 1:10) { #nrow(movie)
-    omdbDF[i,] <- getMovieFromOMDB(i);
+  for(i in 1:nrow(movies)) { #nrow(movies)
+    omdbDF[i,] <- getMovieFromOMDB(movies[i,2]);
+    if(i %% 50 == 0) {
+      print(paste("Download progress by ", round(i*100/nrow(movies), digits = 2), "%")  )
+    }
+    
   }
   return(omdbDF)
 }
 
-getMovieFromOMDB <- function(i) {
-  callURL <- getUrl(movie[i,2]);
+# make API call with movie from row i
+getMovieFromOMDB <- function(movieID) {
+  callURL <- getUrl(movieID);
   dat <-fromJSON(callURL);
   return(dat);
 }
 
+# build API URL with movie id
 getUrl <- function(id) {
   root <- "http://www.omdbapi.com/?"
   u <- paste0(root,"i=", id, "&tomatoes=true")
@@ -24,5 +30,8 @@ getUrl <- function(id) {
 }
 
 
-widDistrDF = getOMDBMovieData('WIDEDISTR_MOVIES_1972_2016.csv')
-getOMDBMovieData('ALL_MOVIES_2016.csv')
+widDistrDF = getOMDBMovieData('WIDEDISTR_MOVIES_1972_2016.csv');
+write.csv(widDistrDF, "OMDB_WIDEDISTR_MOVIES_1972_2016.csv");
+allMovDF = getOMDBMovieData('ALL_MOVIES_2016.csv');
+write.csv(allMovDF, "OMDB_ALL_MOVIES_2016.csv")
+
