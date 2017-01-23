85a457c2fa8ac4fc22a887f3bfeea6712dd52c89
Michael Ion <michael.antonius.ion@gmail.com>
2017-01-23 21:53:05 +0100

    add quicksave function for API  caller

--- movieAPIconnector.R
+++ movieAPIconnector.R
@@ -5,10 +5,23 @@ getOMDBMovieData <- function(movieSrcName) {
   #create omdb dataframe
   dat <- getMovieFromOMDB(movies[1,2]);
   omdbDF <- as.data.frame(setNames(replicate(35,numeric(0), simplify = F), names(dat)))
-  for(i in 1:nrow(movies)) { #nrow(movies)
-    omdbDF[i,] <- getMovieFromOMDB(movies[i,2]);
+  for(i in 3751:nrow(movies)) { #nrow(movies)
+    dat <- getMovieFromOMDB(movies[i,2]);
+    if(is.na(dat["Error"])){
+      omdbDF[i,] <- dat;
+    } else {
+      i = i - 1;
+      print("BAD API RESPONSE, RETRYING...")
+      Sys.sleep(1);
+    }
+    
     if(i %% 50 == 0) {
       print(paste("Download progress by ", round(i*100/nrow(movies), digits = 2), "%")  )
+    }
+    #tododelete
+    if(i %% 250 == 0) {
+      write.csv(omdbDF, paste("temp/movies_by_i_", i, ".csv"))
+      print(" --- Quicksaved progress! ---")
     }
     
   }
@@ -30,8 +43,8 @@ getUrl <- function(id) {
 }
 
 
-widDistrDF = getOMDBMovieData('WIDEDISTR_MOVIES_1972_2016.csv');
-write.csv(widDistrDF, "OMDB_WIDEDISTR_MOVIES_1972_2016.csv");
+#widDistrDF = getOMDBMovieData('WIDEDISTR_MOVIES_1972_2016.csv');
+#write.csv(widDistrDF, "OMDB_WIDEDISTR_MOVIES_1972_2016.csv");
 allMovDF = getOMDBMovieData('ALL_MOVIES_2016.csv');
 write.csv(allMovDF, "OMDB_ALL_MOVIES_2016.csv")
 
